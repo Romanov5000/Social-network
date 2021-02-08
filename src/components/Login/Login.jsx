5c06cc170092ff4ca4dd5c14b041a0bc71617149
Romanov5000 <romanovvladislav97@gmail.com>
2021-02-08 23:54:33 +0300

    login form part one

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,63 @@
-import React from 'react';
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
-const Login = (props) =>{
-    return <div>lox</div>
-}
+const Login = (props) => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <UserSearchForm />
+    </div>
+  );
+};
+
+const UserSearchForm = () => {
+  const validateForm = (values: any) => {
+    const errors = {};
+    return errors;
+  };
+
+  const onSubmitForm = (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 400);
+  };
+  return (
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validate={validateForm}
+      onSubmit={onSubmitForm}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <div>
+            <Field type="login" name="login" />
+            <ErrorMessage name="email" component="div" />
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="div" />
+            <Field type="checkbox" name="rememberMe" />
+            Remember me
+            <ErrorMessage name="rememberMe" component="div" />
+          </div>
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+// const LoginForm = (props) => {
+//   return (
+//     <form >
+//       <div>
+//         <Field placeholder={"Login"} name={'login'} component={'input'}/>
+//         <Field placeholder={"password"} name={'password'} component={'input'}/>
+//         <Field type={"checkbox"} name={'rememberMe'} component={'input'}/> Remember me
+//       </div>
+//       <button>Log in</button>
+//     </form>
+//   );
+// };
 
 export default Login;
